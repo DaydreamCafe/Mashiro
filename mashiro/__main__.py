47de73c820e05272c66e6211516b74d8dbafa81c
AngelicaRoot_na <1423634265@qq.com>
2021-08-23 17:35:24 +0800

    client守护进程

--- mashiro/__main__.py
+++ mashiro/__main__.py
@@ -1,7 +1,31 @@
 # -*- coding:utf-8 -*-
+import logging
+import sys
+import threading
+
+from mashiro.clients.cqhttp_client import CqHttpClient
+from mashiro.utils.config import MashiroConfig
+
 
 class Mashiro:
     @staticmethod
     def run():
         """启动go-cqhttps的守护进程"""
-        pass
+
+        # 启动client线程
+        def _run_client():
+            _config = MashiroConfig().read()
+            client = CqHttpClient(_config['MashiroConfig']['ReactGroup'], _config['cqhttpConfig']['IpAddress'],
+                                  _config['cqhttpConfig']['Port'], _config['cqhttpConfig']['Token'])
+
+        thread = threading.Thread(target=_run_client)
+        thread.setDaemon(True)
+        thread.start()
+
+        # 捕捉Ctrl+C
+        try:
+            while True:
+                pass
+        except KeyboardInterrupt:
+            logging.info('Programme is exiting...')
+            sys.exit(1)

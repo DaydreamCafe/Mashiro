c61a289b46ae749fc9e2d5e2191770fca4fc5bc3
AngelicaRoot_na <1423634265@qq.com>
2021-08-23 18:10:41 +0800

    修复client初始化完成后未启动的问题

--- mashiro/__main__.py
+++ mashiro/__main__.py
@@ -14,14 +14,11 @@ class Mashiro:
 
         # 启动client线程
         def _run_client():
-            # 设置日志格式
-            log_format = "%(asctime)s - %(levelname)s - %(message)s"
-            date_format = "%m/%d/%Y %H:%M:%S %p"
-            logging.basicConfig(filename='my.log', level=logging.DEBUG, format=log_format, datefmt=date_format)
 
             _config = MashiroConfig().read()
             client = CqHttpClient(_config['MashiroConfig']['ReactGroup'], _config['cqhttpConfig']['IpAddress'],
                                   _config['cqhttpConfig']['Port'], _config['cqhttpConfig']['Token'])
+            client.run()
 
         thread = threading.Thread(target=_run_client)
         thread.setDaemon(True)

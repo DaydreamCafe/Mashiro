774ec6525bec110f9e08b0ea9690e1450873f1f3
AngelicaRoot_na <1423634265@qq.com>
2021-08-24 15:21:59 +0800

    增加log信息

--- mashiro/utils/parser_loader.py
+++ mashiro/utils/parser_loader.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
-import logging
 import pkgutil
 
 from mashiro.utils.config import MashiroConfig
+from mashiro.utils.logger import Logger
 
 
 class Parser:
@@ -16,6 +16,9 @@ class Parser:
 
 class ParserLoader:
     def __init__(self):
+        # 初始化logger
+        self.logger = Logger('parser_loader.py')
+
         # 默认自定义解析器位置
         self.custom_parser_path = './mashiro/parsers/custom'
 
@@ -28,23 +31,23 @@ class ParserLoader:
         """加载解析器，返回一个解析器对象"""
         parser = Parser()
 
-        logging.info('Start loading parser...')
+        self.logger.info('Start loading parser...')
         if self.parser_name == 'BuiltIn':
             # 加载内建解析器
-            logging.info('Selected parser: Built-In parser')
+            self.logger.info('Selected parser: Built-In parser')
             from mashiro.parsers import builtin_parser
             parser = builtin_parser.Parser(command_identifier=self.command_identifier)
         else:
             # 加载外部解析器
-            logging.info('Trying to load external parser: {}'.format(self.parser_name))
+            self.logger.info('Trying to load external parser: {}'.format(self.parser_name))
             for finder, name, ispck in pkgutil.walk_packages([self.custom_parser_path]):
                 if name == self.parser_name:
                     loader = finder.find_module(name)
                     parser = loader.load_module(name).Parser(self.command_identifier)
-                    logging.info('Loaded parser {}'.format(self.parser_name))
+                    self.logger.info('Loaded parser {}'.format(self.parser_name))
                 else:
                     # 在没有可用的外部解析器的情况下加载内建解析器
-                    logging.warning('Parser {} not found, switched to Built-In parser'.format(self.parser_name))
+                    self.logger.warning('Parser {} not found, switched to Built-In parser'.format(self.parser_name))
                     from mashiro.parsers import builtin_parser
                     parser = builtin_parser.Parser(command_identifier=self.command_identifier)
 

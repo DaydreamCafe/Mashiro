9c8355502b6cf5a4f7a7bf69a2b264017ce5d2ba
AngelicaRoot_na <1423634265@qq.com>
2021-08-24 15:21:43 +0800

    增加log信息

--- mashiro/clients/cqhttp_client.py
+++ mashiro/clients/cqhttp_client.py
@@ -2,13 +2,14 @@
 """
 cqhttp的客户端
 """
-import logging
 import sys
 import threading
+import time
 
 import func_timeout
 import websocket
 
+from mashiro.utils.logger import Logger
 from mashiro.utils.parser_loader import ParserLoader
 from mashiro.utils.plugin_loader import PluginLoader
 from mashiro.utils.plugin_manager import MashiroPlugin
@@ -20,10 +21,8 @@ from mashiro.utils.config import MashiroConfig
 
 class CqHttpClient(websocket.WebSocketApp):
     def __init__(self, react_group_id: int, ip: str, port: int, token: str):
-        # 初始化log模块
-        log_format = "%(asctime)s - %(levelname)s - %(message)s"
-        date_format = "%m/%d/%Y %H:%M:%S %p"
-        logging.basicConfig(filename='my.log', level=logging.DEBUG, format=log_format, datefmt=date_format)
+        # 初始化Logger
+        self.logger = Logger('cq_http_client.py')
 
         # Websocket基本设置
         self.ws_address = ip
@@ -32,59 +31,76 @@ class CqHttpClient(websocket.WebSocketApp):
         self.react_group_id = react_group_id
         self.client_to_query_stats = '1'
         self.client_to_query_online = '2'
-        websocket.enableTrace(True)
+        # websocket.enableTrace(True)
         url = f'ws://{self.ws_address}:{self.ws_port}/'
         if self.access_token != '':
             url += '?access_token={}'.format(self.access_token)
 
         # 连接至Websocket服务器
-        logging.info('Now connecting to {}'.format(url))
+        self.logger.info('Now connecting to {}'.format(url))
         super().__init__(url, on_message=self.on_message)
 
         # 加载字符串解析器
-        logging.info('Parser loading phase')
+        self.logger.debug('Parser loading phase')
         self.parser = ParserLoader().load()
+        self.logger.debug('Parser loading finished')
 
         # 安装插件
-        logging.info('Plugin installing phase')
+        self.logger.debug('Plugin installing phase')
         MashiroPlugin().install_all()
+        self.logger.debug('Plugin installation finished')
 
         # 加载插件
-        logging.info('Plugin loading phase')
+        self.logger.debug('Plugin loading phase')
         self.plugin_list, self.on_start_func, self.on_trigger_command, self.on_active_command = PluginLoader().load()
+        self.logger.debug('Plugin loading finished')
+        self.logger.debug('plugin_list: {}'.format(self.plugin_list))
+        self.logger.debug('on_start_func: {}'.format(self.on_start_func))
+        self.logger.debug('on_active_command: {}'.format(self.on_active_command))
 
         # 初始化消息栈
+        self.logger.debug('Init message stack')
         self.msg_stack = MessageStack(self.send)
+        self.logger.debug('Message stack initialization finished')
 
         # 初始化统一插件接口
+        self.logger.debug('Init plugin interface')
         self.client = Interface(react_group=self.react_group_id, add_msg=self.msg_stack.add, close=exit,
                                 on_message_func=self.on_active_command, plugin_list=self.plugin_list,
                                 load_plugin=self.load_plugins)
+        self.logger.debug('Plugin interface initialization finished')
 
     def load_plugins(self):
         # 加载插件
-        logging.info('Plugin loading phase')
+        self.logger.debug('Loading plugins')
         self.plugin_list, self.on_start_func, self.on_trigger_command, self.on_active_command = PluginLoader().load()
+        self.logger.debug('Plugin loading finished')
 
     def run(self):
+        self.logger.debug('Get ready for running go-cqhttp client')
+
         def on_start_thread(*args):
             """插件on_start函数线程启动函数"""
+            self.logger.debug('New thread for {}'.format(args[0]['plugin_id']))
             args[0]['target_func'](args[1])
 
         # 启动on_start函数线程
+        self.logger.debug('Start on start functions...')
         for on_start_func in self.on_start_func:
             thread = threading.Thread(target=on_start_thread, args=(on_start_func, self.client))
             thread.setDaemon(True)
             thread.start()
 
         # 启动go-cqhttp连接
+        time.sleep(0.1)
+        self.logger.debug('Start running go-cqhttp client')
         self.run_forever()
 
-    def on_message(self, data):
+    def on_message(self, _, message):
         """消息调起的函数"""
         try:
             # 解析原始数据
-            raw_msg, args = self.parser.parse(data)
+            raw_msg, args = self.parser.parse(message)
             # 初始化专用插件接口
             client = Interface(react_group=self.react_group_id, add_msg=self.msg_stack.add, close=exit,
                                on_message_func=self.on_active_command, plugin_list=self.plugin_list,
@@ -95,14 +111,14 @@ class CqHttpClient(websocket.WebSocketApp):
 
             # 调用内置指令
             if args[0] in built_in.registered_command.keys():
-                @func_timeout.func_set_timeout(MashiroConfig().read()['CommandConfig']['CommandIdentifier'])
+                @func_timeout.func_set_timeout(MashiroConfig().read()['CommandConfig']['MaxResponseTime'])
                 def run_command():
                     built_in.registered_command[args[0]](client)
 
                 try:
                     run_command()
                 except func_timeout.exceptions.FunctionTimedOut:
-                    logging.warning('[WatchDog]Killed processing command {}'.format(args[0]))
+                    self.logger.warning('[WatchDog]Killed processing command {}'.format(args[0]))
 
             # 调用钩子指令
             for command in self.on_trigger_command:
@@ -114,7 +130,7 @@ class CqHttpClient(websocket.WebSocketApp):
                     try:
                         run_command()
                     except func_timeout.exceptions.FunctionTimedOut:
-                        logging.warning('[WatchDog]Killed processing command {}'.format(args[0]))
+                        self.logger.warning('[WatchDog]Killed processing command {}'.format(args[0]))
 
             # 调用主动式指令
             for command in self.on_active_command:
@@ -126,7 +142,7 @@ class CqHttpClient(websocket.WebSocketApp):
                     try:
                         _run_command()
                     except func_timeout.exceptions.FunctionTimedOut:
-                        logging.warning('[WatchDog]Killed processing command {}'.format(args[0]))
+                        self.logger.warning('[WatchDog]Killed processing command {}'.format(args[0]))
 
                 thread = threading.Thread(target=command_thread)
                 thread.setDaemon(True)
@@ -139,5 +155,5 @@ class CqHttpClient(websocket.WebSocketApp):
     def exit(self):
         """结束Websocket Client"""
         self.close()
-        logging.info('Closed connection.Exiting client...')
+        self.logger.info('Closed connection.Exiting client...')
         sys.exit(0)

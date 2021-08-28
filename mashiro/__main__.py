fb9872695d782b916b37308bddd6d255fc2c85de
White_Paper_ <1423634265@qq.com>
2021-08-28 10:18:03 +0800

    log程序PID

--- mashiro/__main__.py
+++ mashiro/__main__.py
@@ -1,9 +1,15 @@
 # -*- coding:utf-8 -*-
+import os
+
+from mashiro.utils.logger import Logger
 from mashiro.clients.cqhttp_client import CqHttpClient
 from mashiro.utils.config import MashiroConfig
 
 
 class Mashiro:
+    logger = Logger('__main__.py')
+    logger.info('Mashiro is running(PID: {})'.format(os.getpid()))
+
     @staticmethod
     def run():
         """启动go-cqhttps的进程"""

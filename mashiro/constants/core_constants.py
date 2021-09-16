388719d444de540ff1f7964605164f153a88910c
White_Paper_ <1423634265@qq.com>
2021-09-16 16:56:54 +0800

    一些核心常量

---
+++ mashiro/constants/core_constants.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+"""
+核心常量
+"""
+
+import os.path
+
+# Name info
+name = 'Mashiro'
+name_full = 'MashiroBot'
+name_chn = '真白'
+mame_jpn = 'ましろ'
+package_name = 'mashiro'
+
+# Author info
+author = 'WhitePaper'
+author_id = 'WhitePaper233'
+author_chn = '白芷'
+author_github = 'https://github.com/WhitePaper233'
+author_bilibili = 'https://space.bilibili.com/22433608'
+author_blog = 'https://whitepaper233.gitee.io/'
+
+# Version info
+version = '2.0.1'
+version_inside = 'a20210916001'
+
+# Repo info
+org_github = 'https://github.com/BotMashiro'
+project_github = 'https://github.com/BotMashiro/Mashiro'
+github_api_latest = 'https://api.github.com/repos/BotMashiro/Mashiro/releases/latest'
+
+# Output channel
+output_channel_limit = 128
+output_interval = 0.1
+
+# Path
+path = os.getcwd()
+
+# Log
+log_path = os.path.join(path, 'logs')
